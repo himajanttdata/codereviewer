e5a8f52ec04e19e175483b09d20088bf23b67379
himajanttdata <338574@nttdata.com>
2025-04-23 21:35:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfil
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
     esbuildOptions: {
       define: {
